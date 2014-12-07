1a4aea91c529a7751c08aec8562aa7958c593ee3
Phil Pirozhkov <pirjsuka@gmail.com>
2014-12-08 00:34:12 +0300

    redirection with overwrite does not work: drop it

--- init.sh
+++ init.sh
@@ -10,7 +10,7 @@ touch .ssh/authorized_keys
 chmod 600 .ssh/authorized_keys
 OWNER_PUBKEY=$(<owner.pub)
 
-echo <<OwnerAndPublic >! .ssh/authorized_keys
+echo <<OwnerAndPublic > .ssh/authorized_keys
 no-agent-forwarding,no-port-forwarding,no-pty,no-user-rc,no-X11-forwarding,command="bin/access/public \$SSH_ORIGINAL_COMMAND" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICCK1i7GAIEE7e0b+8lcaiUkKDmcMZk2hbSXcGnxd08a public@anyone
 no-agent-forwarding,no-port-forwarding,no-pty,no-user-rc,no-X11-forwarding,command="bin/access/owner \$SSH_ORIGINAL_COMMAND" $OWNER_PUBKEY
 OwnerAndPublic
